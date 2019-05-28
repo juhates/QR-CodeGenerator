17dc12c043bae30687f659325f8951782f8fee03
juha <juhates@gmail.com>
2019-05-29 00:53:18 +0300

    deleted one comment

--- generator.py
+++ generator.py
@@ -1,13 +1,13 @@
 import pyqrcode
 
 def qrcode():
-    index = 0   # File name
-    file = open('qr-codes.txt')     # File path
+    index = 0  
+    file = open('qr-codes.txt') # File path
     for row in file:
         qr = pyqrcode.create(row)
         qr.png(str(index) + '.png', scale=6)
         index = index + 1
     print('QR Code Generation Completed...')
-    
+
 if __name__ == "__main__":
     qrcode()
